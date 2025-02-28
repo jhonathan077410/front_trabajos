b6c8ab4425043f6fea427dfeee277bfa4e9746ec
jhonathan077410 <jfcastanedac@itc.edu.co>
2025-02-28 04:48:56 +0000

    feat:se crea el menu

---
+++ front_trabajoso/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import MenuPrincipal from './components/Menu.vue'
+</script>
+
+<template>
+    <div class="container-fluid">
+        <MenuPrincipal/>
+    </div>
+    <HelloWorld/>
+  
+</template>
+
+<style scoped>
+
+</style>
